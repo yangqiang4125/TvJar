d1a7e157aa9efedb545043571caf8e5237d1f886
yangqiang3504 <412594121@qq.com>
2022-09-01 17:48:26 +0800

    add - bili899

--- app/src/main/java/com/github/catvod/utils/okhttp/OkHttpUtil.java
+++ app/src/main/java/com/github/catvod/utils/okhttp/OkHttpUtil.java
@@ -3,20 +3,20 @@ package com.github.catvod.utils.okhttp;
 import com.github.catvod.crawler.SpiderDebug;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.*;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 
 public class OkHttpUtil {
 
     public static final String METHOD_GET = "GET";
     public static final String METHOD_POST = "POST";
 
-    private static final int DEFAULT_TIMEOUT = 15;
+    private static final int DEFAULT_TIMEOUT = 25;
 
     private static final Object lockO = new Object();
 
@@ -27,24 +27,10 @@ public class OkHttpUtil {
      */
     private static OkHttpClient noRedirectClient = null;
 
-    public static HashMap<String, List<Cookie>> cookieStore = new HashMap<>();
-
     public static OkHttpClient defaultClient() {
         synchronized (lockO) {
             if (defaultClient == null) {
                 OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                        .cookieJar(new CookieJar() {
-                            @Override
-                            public void saveFromResponse(HttpUrl httpUrl, List<Cookie> list) {
-                                cookieStore.put(httpUrl.host(), list);
-                            }
-
-                            @Override
-                            public List<Cookie> loadForRequest(HttpUrl httpUrl) {
-                                List<Cookie> cookies = cookieStore.get(httpUrl.host());
-                                return cookies != null ? cookies : new ArrayList<>();
-                            }
-                        })
                         .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
                         .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
                         .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
