29cab6a7f6946f04cf66b4f2430c699706db3406
yangqiang3504 <412594121@qq.com>
2022-10-09 15:06:23 +0800

    add - douban 格式1234

--- app/src/main/java/com/github/catvod/spider/Zhaozy.java
+++ app/src/main/java/com/github/catvod/spider/Zhaozy.java
@@ -1,5 +1,7 @@
 package com.github.catvod.spider;
 
+import android.content.Context;
+import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
 import com.github.catvod.utils.okhttp.OkHttpUtil;
 import org.json.JSONArray;
@@ -10,29 +12,41 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.net.URLEncoder;
-import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class Zhaozy extends PushAgent {
+public class Zhaozy extends Spider {
+    private PushAgent pushAgent;
     private static String b = "https://zhaoziyuan.me/";
-    protected static HashMap<String, String> sHeaders() {
-        HashMap<String, String> headers = new HashMap<>();
-        headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.62 Safari/537.36");
-        headers.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-        headers.put("Accept-encoding", "gzip, deflate, br");
-        headers.put("Accept-language", "zh-SG,zh;q=0.9,en-GB;q=0.8,en;q=0.7,zh-CN;q=0.6");
-        headers.put("Referer", b);
-        return headers;
+    @Override
+    public void init(Context context, String str) {
+        super.init(context, str);
+        pushAgent = new PushAgent();
+        pushAgent.init(context, str);
     }
+    @Override
+    public String detailContent(List<String> list) {
+        try {
+            return pushAgent.detailContent(list);
+        } catch (Exception e) {
+            SpiderDebug.log(e);
+        }
+        return "";
+    }
+    @Override
+    public String playerContent(String str, String str2, List<String> list) {
+        return pushAgent.playerContent(str, str2, list);
+    }
+
     private Pattern regexVid = Pattern.compile("(\\S+)");
 
     @Override
     public String searchContent(String key, boolean quick) {
         try {
             String url = "https://zhaoziyuan.me/so?filename=" + URLEncoder.encode(key);
-            Document docs = Jsoup.parse(OkHttpUtil.string(url, sHeaders()));
+            Document docs = Jsoup.parse(OkHttpUtil.string(url, null));
             JSONObject result = new JSONObject();
             JSONArray videos = new JSONArray();
             Elements list = docs.select("div.li_con div.news_text");
@@ -49,7 +63,7 @@ public class Zhaozy extends PushAgent {
                         String remark = doc.select("div.news_text a p").text();
                         remark = remark.replaceAll(".*收录时间：(.*)", "$1");
                         v.put("vod_id", id + "$$$" + pic + "$$$" + title);
-                        v.put("vod_name", title.trim());
+                        v.put("vod_name", title);
                         v.put("vod_pic", pic);
                         v.put("vod_remarks", remark);
                         videos.put(v);
